db828f0d4f5585ed577cca3417e0f5995b4b7b15
Joel McCandless <mail@joelmccandless.com>
2024-11-09 14:59:23 -0500

    add text to speech and speech to text

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from load_firebase import experiences
+from text_to_speech import text_to_speech
+from speech_to_text import speech_to_text
 
 app = Flask(__name__)
 
@@ -12,13 +14,21 @@ def home():
 # TODO: include conversation with ChatGPT and maps API
 @app.route('/query')
 def query():
-   return 'query!'
+    return 'query!'
 
 # record an experience
 # TODO: include conversation with ChatGPT and maps API
 @app.route('/record')
 def record():
-   return 'record!'
+    return 'record!'
+
+# tests encoding text to speech and speech to text
+@app.route('/test')
+def test():
+    audio = text_to_speech('hi this is a test').audio_content
+    text_results = speech_to_text(audio)
+    print(text_results)
+    return 'done'
 
 if __name__ == '__main__':  
-   app.run()
+    app.run()
---
+++ backend/text_to_speech.py
@@ -0,0 +1,15 @@
+from google.cloud import texttospeech
+
+tts_client = texttospeech.TextToSpeechClient()
+
+def text_to_speech(text):
+    # configure request
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+    # TODO: have users select voice gender
+    voice = texttospeech.VoiceSelectionParams(language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
+
+    # Generate the audio content
+    response = tts_client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
+
+    return response
